ae1d75f0c9a8f84364c5bd5dd58a025b60fbbdd9
George Kadianakis <desnacked@riseup.net>
2022-06-06 11:56:57 +0200

    Turn Sample into a dataclass.
    That's what consensus-specs uses and it kills the __init__() code

--- shared.py
+++ shared.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from py_ecc import optimized_bls12_381 as b
 from py_ecc.fields import optimized_bls12_381_FQ as FQ
 from py_ecc.typing import Optimized_Point3D
@@ -9,19 +11,12 @@ from setup import get_setup
 
 MODULUS = b.curve_order
 
-
-class Sample:
+@dataclass
+class Sample():
     i: int
     j: int
     vs: [int]
     proof: Optimized_Point3D[FQ]
-
-    def __init__(self, i: int, j: int, vs: [int], proof: Optimized_Point3D[FQ]) -> None:
-        self.i = i
-        self.j = j
-        self.vs = vs
-        self.proof = proof
-
 
 def get_coset_factor(j: int, N_locs: int) -> int:
     order = len(get_setup()[0])
